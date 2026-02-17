be02559813564f7feeb9658a3797af71ca65b4ca
Alison Zerbe <alisonz@indeed.com>
2026-02-16 17:35:23 -0800

    loads grammar in nltk and parses

--- hw6_semantics.py
+++ hw6_semantics.py
@@ -1,5 +1,7 @@
 import sys
 import os
+import nltk
+from nltk import grammar, parse
 
 def get_inputs():
     grammar_input = sys.argv[1]
@@ -11,16 +13,29 @@ def get_inputs():
     
     return grammar_input, input_sentences, output_file
 
-def read_sentences(input_sentences, output_file):
+def load_grammar(grammar_input):
+    abs_path = os.path.abspath(grammar_input)
+    grammar_url = f"file://{abs_path}"
+    print(f"PATHHHHH {grammar_url}")
+    grammar = nltk.data.load(grammar_url)
+    parser = parse.FeatureChartParser(grammar)
+    return parser
+
+def read_sentences(input_sentences, output_file, parser):
     with open(input_sentences, 'r', encoding='utf8') as file:
         lines = file.readlines()
         with open(output_file, "a") as output:
             for line in lines:
-                print(f"XXXX {line}", file=output)
+                if line:
+                    tokens = line.split()
+                    trees = parser.parse(tokens)
+                    print(f"{line}", file=output)
+                    print(f"XXX {tokens}")
 
 def main():
     grammar_input, input_sentences, output_file = get_inputs()
-    read_sentences(input_sentences, output_file)
+    parser = load_grammar(grammar_input)
+    read_sentences(input_sentences, output_file, parser)
 
 if __name__ == '__main__':
     main()
